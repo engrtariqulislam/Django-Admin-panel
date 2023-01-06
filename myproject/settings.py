d9c792cb57f169bf9a6728b96a17b2a45c2a997b
Tariqul islam <tnawayn83@yahoo.com>
2023-01-06 20:27:34 +0300

    3.application included in django project url
    application included in django project url

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp', # application installed in django project
+
 ]
 
 MIDDLEWARE = [
