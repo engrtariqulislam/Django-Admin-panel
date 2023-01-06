5aa5ca8d6123633303c280a3a2dc10e629be8a56
Tariqul islam <tnawayn83@yahoo.com>
2023-01-06 21:26:18 +0300

    5.Add models in Django Admin Panel
    Add models in Django Admin Panel

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#Model Created
+
+class Teacher(models.Model):
+	Firstname = models.CharField(max_length=50)
+	Lastname = models.CharField(max_length=50)
+	Email = models.EmailField(max_length=50)
+	Contacr = models.CharField(max_length=50)
+
+#this function is user for converting object to 
+def __str__(self) -> str:
+	return self.Firstname
