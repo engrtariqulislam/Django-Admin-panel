79c6dfd5aa1b89643cf0e6b2e6c1e767f40021c0
Tariqul islam <tnawayn83@yahoo.com>
2023-01-06 22:19:47 +0300

    7.loding template 2
    loding template 2

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     
     
-   path("",views.IndexView,name="index"),
-   
+  # path("",views.IndexView,name="index"),
+   path("",views.FromView,name="froms")
    
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def IndexView(request): #Index. html View
 	return render(request,"app/index.html")
+
+def FromView(request): #Froms. html View
+	return render(request,"app/froms.html")
